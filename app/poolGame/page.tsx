8d01602080f6481763f0877cbb761cee846753e3
sosia24 <sosia24@proton.me>
2025-11-24 14:26:04 -0300

    new commit

--- app/poolGame/page.tsx
+++ app/poolGame/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
     getUserTickIds,
